70e1235ab73f0462551d1980e91349f92c4dd430
Rakib Hasan Bul Bul <hrakib182@gmail.com>
2025-07-26 17:57:53 +0600

    Working on Inputs

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include<string.h>
 
 //Fuction Declarations
+void chatbot();
 void tell_time();
 
 
@@ -14,11 +15,39 @@ int main(){
     tell_time();
 }
 
-//Time Module
 
+//Time Module
 void tell_time(){
     time_t t;
 
     time (&t);
     printf("Current time is %s", ctime(&t));
+}
+
+
+//chatbot
+
+void chatbot(){
+    char input[100];
+
+    while (1){
+        printf("user: \n");
+
+        fgets(input, sizeof(input), stdin);
+        input[strspn(input, "/n")] == 0;
+
+        if (strcmp(input, "Exit") == 0){
+            printf("Good bye /n");
+            break;
+        }
+
+    }
+
+    const char* intent = classify(input);
+
+    if (strcmp(intent, "GREET") == 0){
+        printf("Benga: Hello there \n");
+    }
+
+    printf("It's a bot");
 }
